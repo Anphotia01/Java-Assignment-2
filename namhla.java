6eac391706e1e6877bbe8aafdcbee749129eac3f
Anphotia <84147693+Anphotia@users.noreply.github.com>
2021-05-15 22:17:47 +0200

    Add files via upload

---
+++ namhla.java
@@ -0,0 +1,13 @@
+/* Namhla Qqoboka
+  219423334
+  Java Assignment 2
+*/
+
+import javax.swing.*;
+public class namhla {
+    public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null, "Programming if fun",null, JOptionPane.INFORMATION_MESSAGE);
+     System.exit(0);
+
+    } 
+}
